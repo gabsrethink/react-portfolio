b6b81a8d310e56e80c731dcca97ceea32144daa0
gabriel.melo <gabriel.melo@rethink.dev>
2024-08-09 17:24:16 -0300

    fix: fixing responsive layouts

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -13,10 +13,10 @@ export const Cover: React.FC = () => {
   return (
     <div className="max-xl:px-8 flex max-w-[1024px] mx-auto justify-center flex-col items-start h-screen p-0">
       <p className="text-green-300 text-base pb-4 font-medium">{greeting}</p>
-      <h1 className="text-gray-50 text-7xl max-xsm:text-5xl font-bold pb-2">
+      <h1 className="text-gray-50 text-7xl max-sm:text-5xl font-bold pb-2">
         {name}
       </h1>
-      <h1 className="text-7xl max-xsm:text-5xl font-bold">{headline}</h1>
+      <h1 className="text-7xl max-sm:text-5xl font-bold">{headline}</h1>
       <p className="text-base max-w-[540px] pt-6 pb-10">{description}</p>
       <Button text={buttonText} onClick={handleButtonClick} />
     </div>
